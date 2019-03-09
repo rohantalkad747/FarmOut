6c1589633f89abf7430d82345afb5465305931a9
dzhao777 <45520377+dzhao777@users.noreply.github.com>
2019-03-09 15:44:13 -0500

    Adding menu items

---
+++ menu.js
@@ -0,0 +1,22 @@
+const menu = [
+	{
+		"name": "pizza",
+		"category": "food",
+		"types": ['cheese', 'pepperoni', 'vegan'],
+		"price": 3.00
+	},
+	{
+		"name": "pasta",
+		"category": "food",
+		"types": ['italian', 'greek', 'vegan'],
+		"price": 8.00
+	},
+	{
+		"name": "drinks",
+		"category": "drink",
+		"types": ['coke', 'orange juice', 'root beer'],
+		"price": 3.00
+	}
+];
+// export modules
+module.exports = menu;
